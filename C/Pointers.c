85ee6a45fb6229465deaedd3b5bee8de3f842c03
NK <oscarfu0721@gmail.com>
2023-08-05 15:02:48 +0100

    no files

--- C/Pointers.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-int main() {
-	int x = 5;
-	int *xptr = &x;
-	printf("%p\n", xptr);
-	printf("%d\n", *xptr); // dereferencing
-}
--- C/nky.c
+++ C/nky.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#include "colour.h"
+#include "lib/colour.h"
 
 int main(int argc, char** argv) {
 	if (argc != 2) {
